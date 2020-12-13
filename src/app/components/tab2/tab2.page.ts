97addc87f439049cb4abe1ccaa29349566767bc9
viriyalucky <viriyaluckyoo@gmail.com>
2020-12-13 20:46:43 +0700

    fix maps 2

--- src/app/components/tab2/tab2.page.ts
+++ src/app/components/tab2/tab2.page.ts
@@ -36,7 +36,7 @@ export class Tab2Page implements OnInit {
     const  params = new  HttpParams().set('access_key', "850fadfe5a7d52ff86fc06bbc2053bf2").set('query', lat.toString() + ", " +  lng.toString()).set("output", "json");
 
     return new Promise((resolve,reject)=>{ 
-      this.http.get('http://api.positionstack.com/v1/reverse', {params}).subscribe((result:any)=>{
+      this.http.get('https://api.positionstack.com/v1/reverse', {params}).subscribe((result:any)=>{
         let data : any = result.data;
         let res : any= data.filter(dat=> dat.type == "street")
         let address = res[0].name
@@ -140,7 +140,7 @@ export class Tab2Page implements OnInit {
   //   // console.log(`Distance approx. ${this.distance} meters`);
   //   this.showToast(`Distance approx. ${this.distance} meters`);    
   // }
-  
+
 
   protected mapReady(map) {
     this.map = map;
